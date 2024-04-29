65db5ace2cab166d7e3a6cb413ebf1505c3006c9
mav0716 <dowellj0716@gmail.com>
2024-04-28 19:15:07 -0500

    banner and spinner

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Make sure CommonModule is imported
+import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { ReactiveFormsModule } from '@angular/forms'; 
 import { EmailService } from '../services/email.service';
+import { Message } from 'primeng/api';
+import { MessagesModule } from 'primeng/messages';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule, InputTextModule, ButtonModule, CardModule, ReactiveFormsModule],
+  imports: [CommonModule, InputTextModule, ButtonModule, CardModule, ReactiveFormsModule, MessagesModule, ProgressSpinnerModule],
   providers: [EmailService],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent implements OnInit {
   contactForm!: FormGroup;
+  messages: Message[] = [];
+  showSpinner: boolean = false;
 
   constructor(private formBuilder: FormBuilder, private emailService: EmailService) {}
 
@@ -30,10 +35,26 @@ export class ContactComponent implements OnInit {
   }
 
   onSubmit() {
+    this.showSpinner = true;
     if (this.contactForm.valid) {
       this.emailService.sendEmail(this.contactForm.value).subscribe({
-        next: response => console.log('Email sent successfully!', response),
-        error: error => console.log('Error sending email', error)
+        next: response => {
+          console.log('Email sent successfully!', response);
+          this.messages = [{ severity: 'success', summary: 'Success', detail: 'Email sent successfully!' }];
+          setTimeout(() => {
+            this.messages = []; 
+          }, 15000); 
+          this.contactForm.reset();
+          this.showSpinner = false;
+        },
+        error: error => {
+          console.log('Error sending email', error);
+          this.messages = [{ severity: 'error', summary: 'Error', detail: 'Failed to send email. Please try again.' }];
+          setTimeout(() => {
+            this.messages = []; 
+          }, 15000); 
+          this.showSpinner = false;
+        }
       });
     }  
   }
